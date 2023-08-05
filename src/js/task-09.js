1887e49134cc4cbea50c8da87013e0cf2fe01675
77Vadim77 <125355511+77Vadim77@users.noreply.github.com>
2023-08-05 13:19:10 +0300

    7865789

--- src/js/task-09.js
+++ src/js/task-09.js
@@ -4,6 +4,7 @@ function getRandomHexColor() {
     .padStart(6, 0)}`;
 }
 
+
 const widget = document.querySelector('.widget');
 const colorSpan = widget.querySelector('.color');
 const changeColorButton = widget.querySelector('.change-color');
