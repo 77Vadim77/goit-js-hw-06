7becde82f02fca4a4b497eec9131527c65a02c85
77Vadim77 <125355511+77Vadim77@users.noreply.github.com>
2023-08-11 13:35:51 +0300

    update12930284

--- js/task-06.js
+++ js/task-06.js
@@ -6,10 +6,17 @@ const result = Number(input.getAttribute(`data-length`));
 input.addEventListener('blur', getBorderColor);
 
 function getBorderColor(event) {
-    console.log((event.currentTarget.value))
+    console.log((event.currentTarget.value));
     if (event.currentTarget.value.length === result) {
-        input.classList.remove('valid')
+        input.classList.add('valid');
+        input.classList.remove('invalid');
     } else {
+        input.classList.remove('valid');
         input.classList.add('invalid');
     }
-};
+}
+
+
+
+
+
